a70f52ec9c4f18bf38951107fb027075d267027b
xiulinRYM <xiulin.reyes.mora@est.una.ac.cr>
2026-06-07 23:40:29 -0600

    adding things

--- Astronaut.cpp
+++ Astronaut.cpp
@@ -38,3 +38,46 @@ void Astronaut::setEnergy(int value)
     energy_=(value < 0) ? 0 : value;
 }
 
+void Astronaut::applyDamage(int amount)
+{
+    setHealth(getHealth()-amount);
+}
+
+bool Astronaut::isAlive() const
+{
+    return health_>0;
+}
+
+Module* Astronaut::getCurrentModule() const
+{
+    return currentModule_;
+}
+
+Inventory* Astronaut::getInventory() const
+{
+    return inventory_.get();
+}
+
+void Astronaut::setCurrentModule(Module* module)
+{
+    currentModule_=module;
+}
+
+void Astronaut::move(Module* target)
+{
+    // TODO: verify target is connected to currentModule_
+    setCurrentModule(target);
+}
+
+void Astronaut::takeItem(Item* item)
+{
+//no inventory created
+}
+
+void Astronaut::useItem(Item* item)
+{
+//no inventory created
+}
+
+
+
