b2608186a024def948b13ec7974b8a539a448899
danie <jose.brenes.campos@est.una.ac.c>
2026-06-11 17:31:24 -0600

    ReportGenerator que es la bitacora, corregido ahora si corre

---
+++ ReportGnerator.h
@@ -0,0 +1,38 @@
+//
+// Created by danie on 11/6/2026.
+//
+
+#ifndef PROYECTO_2_PROGRA_2_REPORTGNERATOR_H
+#define PROYECTO_2_PROGRA_2_REPORTGNERATOR_H
+
+#ifndef REPORTGENERATOR_H
+#define REPORTGENERATOR_H
+
+#include <string>
+#include <vector>
+#include "Logger.h"
+
+class ReportGenerator {
+public:
+    ReportGenerator(const std::string& fileName = "final_report.txt");
+    void setMissionResult(const std::string& result);
+    void setFinalStats(int oxygen, int energy, int health, int modules);
+    void generate(const Logger& logger);
+    void saveToFile() const;
+
+private:
+    std::string reportFileName;
+    std::string missionResult;
+    int finalOxygen;
+    int finalEnergy;
+    int finalHealth;
+    int finalModules;
+    std::vector<std::string> savedLog;
+
+    std::string buildReport() const;
+};
+
+#endif
+
+
+#endif //PROYECTO_2_PROGRA_2_REPORTGNERATOR_H
