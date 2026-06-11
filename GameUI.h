e51d273cc1423797c5692dca31bccf1fa5ceeb71
danie <jose.brenes.campos@est.una.ac.c>
2026-06-11 16:41:26 -0600

    GameUI.h para usarse en simelador como sonsola

---
+++ GameUI.h
@@ -0,0 +1,21 @@
+//
+// Created by danie on 11/6/2026.
+//
+
+#ifndef PROYECTO_2_PROGRA_2_GAMEUI_H
+#define PROYECTO_2_PROGRA_2_GAMEUI_H
+
+
+#include <string>
+#include "Astronaut.h"
+
+class GameUI {
+public:
+    void showStatus(const Astronaut& astronaut, int currentTurn,int maxTurns) const;
+    void showMenu() const;
+    void showMessage(const std::string& message) const;
+    int getPlayerInput() const;
+};
+
+
+#endif //PROYECTO_2_PROGRA_2_GAMEUI_H
