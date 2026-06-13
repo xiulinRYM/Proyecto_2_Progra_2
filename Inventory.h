cfebb14338ab5b61f9f420357293f136cf4c8c3d
xiulinRYM <xiulin.reyes.mora@est.una.ac.cr>
2026-06-13 01:00:43 -0600

    change

--- Inventory.h
+++ Inventory.h
@@ -13,9 +13,9 @@ class Inventory
     public:
     Inventory()= default;
     void addItem(Item* item);
-    Item* getItem(int pos) const;
+    [[nodiscard]]Item* getItem(int pos) const;
     void removeItem(int pos);
-    int getSize() const;
+    [[nodiscard]]int getSize() const;
 
     private:
     std::vector<Item* > items_;
